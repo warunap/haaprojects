182249dfb92c510dac5b69dff82dc16b652d9c74
gelnyang <gelnyang@d0820c7c-ebc6-11de-9b30-9f9d59cce923>
2011-01-10 13:45:59 +0000

    article

--- sisopipophp/js/jtool_validator.js
+++ sisopipophp/js/jtool_validator.js
@@ -5,8 +5,8 @@
  * @author:Geln Yang
  */
 (function () {
-	jtool.validator = {isNumeric:function (input) {
-		return (input - 0) == input && input.length > 0;
+	jtool.validator = {isNumeric:function (o) {
+		return !isNaN(parseFloat(o)) && isFinite(o);
 	}};
 	if (!jtool.msg) {
 		jtool.msg = {};
@@ -14,24 +14,29 @@
 	jtool.msg.MSG_CANT_BE_NULL = "\u4e0d\u80fd\u4e3a\u7a7a";
 	jtool.msg.MSG_FORMAT_ERROR = "\u683c\u5f0f\u9519\u8bef";
 	jtool.msg.MSG_MUST_BE_NUMBER = "\u5fc5\u9700\u4e3a\u6570\u5b57";
-	jtool.limiter = {notNull:function (object, name) {
+	jtool.limiter = {limit:function (msg, func, obj) {
+		if (!func()) {
+			if (obj) {
+				$(obj).addClass("error");
+			}
+			throw msg;
+		} else {
+			if (obj) {
+				$(obj).removeClass("error");
+			}
+		}
+	}, notNull:function (object, name) {
 		var obj = $(object);
 		name = name ? name : obj.attr("name");
-		if (obj.val() == "") {
-			obj.addClass("error");
-			throw name + jtool.msg.MSG_CANT_BE_NULL + "!";
-		} else {
-			obj.removeClass("error");
-		}
+		jtool.limiter.limit(name + jtool.msg.MSG_CANT_BE_NULL + "!", function () {
+			return obj.val() != "";
+		}, obj);
 	}, number:function (object, name) {
 		var obj = $(object);
 		name = name ? name : obj.attr("name");
-		if (jtool.validator.isNumeric(obj.val())) {
-			obj.removeClass("error");
-		} else {
-			obj.addClass("error");
-			throw name + jtool.msg.MSG_MUST_BE_NUMBER + "!";
-		}
+		jtool.limiter.limit(name + jtool.msg.MSG_MUST_BE_NUMBER + "!", function () {
+			return jtool.validator.isNumeric(obj.val());
+		}, obj);
 	}};
 })(jtool);
 
