ae156f83220471599e7db07e11daf3fbae74239c
gelnyang <gelnyang@d0820c7c-ebc6-11de-9b30-9f9d59cce923>
2012-05-28 10:35:02 +0000

    optimize program structure

--- HAAProject_WebsiteInfoCollector/src/main/java/org/haaprojects/website/info/Main.java
+++
@@ -1,9 +0,0 @@
-package org.haaprojects.website.info;
-
-public class Main {
-
-	public static void main(String[] args) throws Exception {
-		SiteInfoCollector.main(new String[] { "d:/sites_list.txt" });
-	}
-
-}
