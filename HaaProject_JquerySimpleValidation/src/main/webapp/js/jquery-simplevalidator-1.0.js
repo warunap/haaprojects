f3c8dc42b6280ada33ea184b2764728bf0ce8364
gelnyang <gelnyang@gmail.com>
2012-11-27 16:34:58 +0000

    validator dev 20121127

--- HaaProject_JquerySimpleValidation/src/main/webapp/js/jquery-simplevalidator-1.0.js
+++ HaaProject_JquerySimpleValidation/src/main/webapp/js/jquery-simplevalidator-1.0.js
@@ -1,65 +1,69 @@
 (function($) {
-	function isUndefined(obj) {
+	var isUndefined = function(obj) {
 		return typeof obj === "undefined";
-	}
-	var option = {
-		CLASS_VALID_ERROR : "validerror",
-		ATTR_VALID : "isvalid",
-		validation_error_formaterror : "format error!"
-	}
-
-	function isNativeType(type) {
-		if (isUndefined(window.inputtype)) {
-			window.inputtype = {};
-		}
-		if (isUndefined(window.inputtype[type])) {
+	}, nativetypes = {}, nativeattrs = {}, isNativeType = function(type) {
+		if (isUndefined(nativetypes[type])) {
 			var i = document.createElement("input");
 			i.setAttribute("type", type);
-			window.inputtype[type] = (i.type !== "text");
+			nativetypes[type] = (i.type !== "text");
 		}
-		return window.inputtype[type];
-	}
-	function isNativeAttr(type, attr) {
-		return attr in document.createElement(type);
-	}
-
+		return nativetypes[type];
+	}, isNativeAttr = function(type, attr) {
+		var key = type + "_" + attr;
+		if (isUndefined(nativeattrs[key])) {
+			nativeattrs[key] = (attr in document.createElement(type));
+		}
+		return nativeattrs[key];
+	}, CF = {
+		CLASS_VALID_ERROR : "validerror",
+		ATTR_VALID : "isvalid"
+	}, i18n = {
+		fromat_error : "format error!"
+	}, getText = function(key) {
+		return i18n[key];
+	}, regexMap = {
+		"email" : "^(([_\\-][a-z0-9]+)|([a-z0-9]+))([\\._\\-][a-z0-9]+)*[a-z0-9]*@[a-z0-9\\-]+(\\.[a-z0-9\\-]+)*(\\.[a-z]{2,4})$",
+		"date" : "^\\d{4}/\\d{2}/\\d{2}$",
+		"number" : "^\\d+$",
+		"passwd" : "^.*[a-zA-Z]+.*$",
+		"date" : "^\\d{4}/\\d{1,2}/\\d{1,2}$"
+	},
 	/*-------------------------------------------------
 	getValidMessageBoxId
 	 */
-	function getValidMessageBoxId(item) {
+	getValidMessageBoxId = function getValidMessageBoxId(item) {
 		var msgid = "errormsg" + $(item).attr("name") + $(item).attr("id");
 		msgid = msgid.replace(/\.|\[|\]/g, "_");
 		return msgid;
-	}
+	},
 
 	/*-------------------------------------------------
 	existsValidMessageBox
 	 */
-	function existsValidMessageBox(item) {
+	existsValidMessageBox = function(item) {
 		return $(item).parent().find("lable[id='" + getValidMessageBoxId(item) + "']").length > 0;
-	}
-
-	/*-------------------------------------------------
-	removeValidError
-	 */
-	function removeValidError(item) {
-		$(item).removeAttr(option.ATTR_VALID);
-		removeValidErrorMsg(item);
-	}
+	},
 	/*-------------------------------------------------
 	removeValidErrorMsg
 	 */
-	function removeValidErrorMsg(item) {
+	removeValidErrorMsg = function(item) {
 		item = $(item);
 		var msgid = getValidMessageBoxId(item);
 		item.parent().find("lable[id='" + msgid + "']").remove();
-		item.removeClass(option.CLASS_VALID_ERROR);
-	}
+		item.removeClass(CF.CLASS_VALID_ERROR);
+	},
+	/*-------------------------------------------------
+	removeValidError
+	 */
+	removeValidError = function(item) {
+		$(item).removeAttr(CF.ATTR_VALID);
+		removeValidErrorMsg(item);
+	},
 	/*-------------------------------------------------
 	addValidError
 	 */
-	function addValidError(item, msg) {
-		item.attr(option.ATTR_VALID, false);
+	addValidError = function(item, msg) {
+		item.attr(CF.ATTR_VALID, false);
 		var msgid = getValidMessageBoxId(item);
 		var msgbox = $(item).parent().find("lable[id='" + msgid + "']");
 		if (msgbox.length == 0) {
@@ -67,11 +71,90 @@
 			msgbox.addClass("errormessage").addClass("nopreview");
 		}
 		if (!msg) {
-			msg = option.validation_common_error_formaterror;
+			msg = getText('fromat_error');
 		}
 		msgbox.text(msg);
-		item.addClass(option.CLASS_VALID_ERROR);
+		item.addClass(CF.CLASS_VALID_ERROR);
+	}, inputValidatorChain = [
+	/* required validator */
+	function(item) {
+		var val = item.val();
+		var defaultNullValue = "";
+		if (item.is("select") && !isUndefined(item.attr("nullvalue"))) {
+			defaultNullValue = item.attr("nullvalue");
+		}
+		if (!isUndefined(item.attr("required")) && !isNativeAttr("input", "required")) {
+			if ($.trim(val) == defaultNullValue) {
+				valid = false;
+				var nullmessage = item.attr("requiredmessage");
+				if (isUndefined(nullmessage)) {
+					if (item.is("select")) {
+						nullmessage = getText('please_select');
+					} else {
+						nullmessage = getText('required');
+					}
+				}
+				addValidError(item, nullmessage);
+				return false;
+			}
+		}
+		return true;
+	},
+	/* length validator */
+	function(item) {
+		var minlength = item.attr("minlength");
+		if (!isUndefined(minlength) && minlength != "" && parseInt(minlength) > 0 && !isNativeAttr("input", "minlength")) {
+			if (item.val().length < parseInt(minlength)) {
+				addValidError(item, input.attr("lengtherrormessage"));
+				return false;
+			}
+		}
+		return true;
+	},
+	/* pattern validator */
+	function(item) {
+		var pattern = item.attr("pattern");
+		if (!isUndefined(pattern) && pattern != "" && !isNativeAttr("input", "pattern")) {
+			if (pattern.indexOf("^") != 0 && !pattern.indexOf("$") != pattern.length) {
+				pattern = "^" + pattern + "$";
+			}
+			var regex = new RegExp(pattern, "g");
+			if (!regex.test(val)) {
+				addValidError(item, input.attr("patternerrormessage"));
+				return false;
+			}
+		}
+		return true;
+	},
+	/* type validator */
+	function(item) {
+		var type = item.attr("type");
+		if (!isUndefined(type) && type != "" && !isNativeType(type)) {
+			type = type.toLowerCase();
+			var regex = regexMap[type];
+			if (regex != null && regex != "") {
+				if (!new RegExp(regex, "g").test(item.val())) {
+					addValidError(item, item.attr("typeerrormessage"));
+					return false;
+				}
+			}
+		}
+		return true;
+	} ];
+	/*-------------------------------------------------
+	validateInput
+	 */
+	function validateInput(input) {
+		for ( var i in inputValidatorChain) {
+			if (!inputValidatorChain[i].call(this, $(input))) {
+				$(input).attr(CF.ATTR_VALID, false);
+				return false;
+			}
+		}
+		removeValidError(input);
+		return true;
 	}
+
 	/*-------------------
 	triggerValidateInput
 	 */
@@ -85,162 +168,14 @@
 					try {
 						validateInput(item);
 					} catch (e) {
+						alert(e);
 					}
 					item.removeAttr("validateprocessing");
 				}, 10);
 			}
 		}
 	}
-	/*-------------------------------------------------
-	validateInput
-	 */
-	function validateInput(input) {
-		var valid = true;
-		var item = $(input);
-		item.attr(option.ATTR_VALID, true);
-		var val = item.val();
-		/*----valid null-----*/
-		var required = item.attr("required");
-		var defaultNullValue = "";
-		if (item.is("select")) {
-			defaultNullValue = item.attr("nullvalue");
-		}
-		var msg = null;
-		if (!isUndefined(required) && isNativeAttr("input", "required")) {
-			if ($.trim(val) == defaultNullValue) {
-				valid = false;
-				var nullmessage = item.attr("nullmessage");
-				if (typeof nullmessage == "undefined") {
-					if (item.is("select")) {
-						nullmessage = i18n.notice_common_info_pleaseSelect;
-					} else {
-						nullmessage = i18n.validation_common_error_notnull;
-					}
-				}
-				addValidError(item, nullmessage);
-				return false;
-			}
-			if (required == "true" && $.trim(val) == defaultNullValue) {
-				removeValidError(item);
-				return true;
-			}
-		}
-		if (val == null) {
-			val = "";
-		}
-		/*----valid format or type-----*/
-		var dataformat = item.attr("dataformat");
-		var datatype = item.attr("datatype");
-		if (typeof dataformat != "undefined" && dataformat != "") {
-			if (dataformat.indexOf("^") != 0 && !dataformat.indexOf("$") != dataformat.length) {
-				dataformat = "^" + dataformat + "$";
-			}
-			var regex = new RegExp(dataformat, "g");
-			if (!regex.test(val)) {
-				valid = false;
-			}
-		} else {
-			if (typeof datatype != "undefined" && datatype != "") {
-				datatype = datatype.toLowerCase();
-				var regex = null;
 
-				switch (datatype) {
-				case "email":
-					val = val.toLowerCase();
-					regex = "^(([_\\-][a-z0-9]+)|([a-z0-9]+))([\\._\\-][a-z0-9]+)*[a-z0-9]*@[a-z0-9\\-]+(\\.[a-z0-9\\-]+)*(\\.[a-z]{2,4})$";
-					break;
-				case "date":
-					regex = "^\\d{4}/\\d{2}/\\d{2}$";
-					break;
-				case "number":
-					regex = "^\\d+$";
-					break;
-				case "passwd":
-					regex = "^.*[a-zA-Z]+.*$";
-					break;
-				case "cardnum":
-					regex = "^((\\d{8})|(\\d{10})|(20\\d{11})|(80[123]01\\d{11}))$";
-					break;
-				case "cardnum16":
-					regex = "^80[123]01\\d{11}$";
-					break;
-				case "serialnum":
-					regex = "^\\d{8}$";
-					break;
-				case "twnumberdate":
-					regex = "^\\d{6}$";
-					break;
-				case "twmobilephone":
-					regex = "^09\\d{8}$";
-					break;
-				default:
-					regex = null;
-				}
-
-				if (datatype == "passwd" && (val.length < 6 || val.length > 15)) {
-					valid = false;
-				}
-
-				if (valid && regex != null && regex != "") {
-					if (!new RegExp(regex, "g").test(val)) {
-						valid = false;
-					}
-				}
-				if (datatype == "cardnum16") {
-					valid = (validateCardNum16(val));
-					if (!valid) {
-						msg = i18n.validation_common_error_cardformaterror;
-					}
-				}
-				if (datatype == "idno") {
-					valid = (checkIdNo(item[0]) || validateTaiwanIdNum(val));
-					if (!valid) {
-						msg = i18n.validation_common_error_invalididno;
-					}
-				} else if (datatype == "idnonative") {
-					valid = (validateTaiwanIdNum(val));
-					if (!valid) {
-						msg = i18n.validation_common_error_invalididno;
-					}
-				} else if (datatype == "idnoforeign") {
-					valid = (checkIdNo(item[0]));
-					if (!valid) {
-						msg = i18n.validation_common_error_invalididno;
-					}
-				}
-
-				if (valid && datatype == "serialnum") {
-					valid = checkSerialnum(val);
-				}
-
-				if (valid && datatype == "twnumberdate") {
-					valid = checkBirth(val);
-				}
-
-			}
-		}
-
-		if (valid) {
-			var limittextsize = item.attr("limittextsize");
-			if (typeof limittextsize != "undefined" && limittextsize != "") {
-				var count = countSubString(val, "\n");
-				var length = stringBytesLength(val) + count;
-				if (length > parseInt(limittextsize)) {
-					valid = false;
-					msg = i18n.validation_common_error_limit;
-				}
-			}
-		}
-
-		if (valid) {
-			removeValidError(item);
-			/* add valid attribute to avoid validating twice */
-			item.attr(ATTR_VALID, true);
-		} else {
-			addValidError(item, msg);
-		}
-		return valid;
-	}
 	function bind(obj, func) {
 		if (!isBinded(obj)) {
 			$(obj).attr("sv_binded", true);
@@ -265,19 +200,18 @@
 	}
 	function bindForm(form) {
 		bind(form, function() {
-			$(form).find("input:not([.notvalid][type=button][type=submit]),select:not([.notvalid][type=button][type=submit])").each(function() {
+			$(form).find("input:not(.notvalid[type=button][type=submit]),select:not(.notvalid[type=button][type=submit])").each(function() {
 				bindInput(this);
 			});
 		});
 	}
-	var SimpleValidator = window.SimpleValidator = {};
 	$.fn.extend({
 		bindSimpleValidator : function() {
 			var obj = $(this);
 			if (obj.is("input") || obj.is("select")) {
-				SimpleValidator.bindInput(obj);
+				bindInput(obj);
 			} else if (obj.is("form")) {
-				SimpleValidator.bindForm(obj);
+				bindForm(obj);
 			}
 		}
 	});
