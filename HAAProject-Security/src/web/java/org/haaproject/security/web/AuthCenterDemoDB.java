ea964ed8bd5b206be53f09c9dcb55a332d9af136
gelnyang@gmail.com <gelnyang@gmail.com@d0820c7c-ebc6-11de-9b30-9f9d59cce923>
2012-12-19 00:51:45 +0000

    initial demoapp

---
+++ HAAProject-Security/src/web/java/org/haaproject/security/web/AuthCenterDemoDB.java
@@ -0,0 +1,23 @@
+/**
+ *      Project:HAAProject-Security       
+ *    File Name:LoginSenarioTest.java	
+ * Created Time:2012-12-13下午11:46:03
+ */
+package org.haaproject.security.web;
+
+
+/**
+ * 
+ * 
+ * @author Geln Yang
+ * 
+ */
+public class AuthCenterDemoDB {
+
+
+	public static String getDESByAppName(String appName) {
+		return "des"+appName;
+	}
+
+
+}
