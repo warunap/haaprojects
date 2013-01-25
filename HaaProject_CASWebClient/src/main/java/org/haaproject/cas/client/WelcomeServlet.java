213daa4726c89dbb8c9a0544a62f02cfec47340c
gelnyang@gmail.com <gelnyang@gmail.com@d0820c7c-ebc6-11de-9b30-9f9d59cce923>
2013-01-25 06:27:39 +0000

    CASWebClient

--- HaaProject_CASWebClient/src/main/java/org/haaproject/cas/client/WelcomeServlet.java
+++ HaaProject_CASWebClient/src/main/java/org/haaproject/cas/client/WelcomeServlet.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jasig.cas.client.authentication.AttributePrincipal;
@@ -27,12 +28,12 @@ public class WelcomeServlet extends HttpServlet {
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		HttpServletRequestWrapper reqWrapper = new HttpServletRequestWrapper(req);
 		PrintWriter writer = resp.getWriter();
-		String remoteUser = req.getRemoteUser();
-		writer.write("removeUser:" + remoteUser + " , ");
+		String remoteUser = reqWrapper.getRemoteUser();
+		writer.write("remoteUser:" + remoteUser + " , ");
 
-		AttributePrincipal principal = (AttributePrincipal) req.getUserPrincipal();
+		AttributePrincipal principal = (AttributePrincipal) reqWrapper.getUserPrincipal();
 		Map<?, ?> attrs = principal.getAttributes();
 		if (attrs == null) {
 			throw new ServletException("no by the CAS client attributes ");
