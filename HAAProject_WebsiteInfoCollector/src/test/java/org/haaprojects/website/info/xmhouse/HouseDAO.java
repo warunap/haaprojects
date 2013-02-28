03f0f43e181f9f72f319bcebb8bf4a006571995f
gelnyang@gmail.com <gelnyang@gmail.com@d0820c7c-ebc6-11de-9b30-9f9d59cce923>
2013-02-28 01:27:34 +0000

    house

---
+++ HAAProject_WebsiteInfoCollector/src/test/java/org/haaprojects/website/info/xmhouse/HouseDAO.java
@@ -0,0 +1,139 @@
+/**
+ *      Project:HAAProject_WebsiteInfoCollector       
+ *    File Name:BuildingDetailDAO.java	
+ * Created Time:2013-2-27下午10:45:07
+ */
+package org.haaprojects.website.info.xmhouse;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.haaprojects.website.info.xmhouse.domain.HouseDetail;
+
+/**
+ * @author Geln Yang
+ * 
+ */
+public class HouseDAO {
+
+	private static final String SPLITOR = "\\|\\|";
+
+	private static String encoding = "UTF-8";
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		saveDataFromTextFile();
+		System.out.println("over");
+
+	}
+
+	/**
+	 * @throws Exception 
+	 * 
+	 */
+	public static void saveDataFromTextFile() throws Exception {
+		File file = new File("e:/housedata/xmhouse_house_detail_list.txt");
+		List<String> lines = FileUtils.readLines(file, encoding);
+		Connection connection = getJDBCConnection();
+		for (String line : lines) {
+			saveHouseTextInfoLine(connection,line);
+
+		}
+		connection.close();		
+	}
+
+	/**
+	 * @param connection
+	 * @param line
+	 */
+	public static void saveHouseTextInfoLine(Connection connection, String line) {
+		HouseDetail houseDetail = new HouseDetail();
+		String[] arr = line.split(SPLITOR);
+		String id = arr[0];
+		String address = arr[1];
+
+		houseDetail.setId(id);
+		houseDetail.setAddress(address);
+
+		for (int i = 2; i < arr.length; i++) {
+			houseDetail.setFieldValue(arr[i]);
+		}
+
+		saveHouseDetail(connection, houseDetail);
+	}
+
+	/**
+	 * @param connection
+	 * @param houseDetail
+	 */
+	public static void saveHouseDetail(Connection connection, HouseDetail houseDetail) {
+		String id = houseDetail.getId();
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet query = statement.executeQuery("select * from house_detail where house_id='" + id + "'");
+			if (query.next()) {
+				String updateSql = "update house_detail set price=" + houseDetail.getPrice() + " , house_limit='" + houseDetail.getLimit() + "'";
+				System.out.println(updateSql);
+				statement.executeUpdate(updateSql);
+			} else {
+				StringBuffer insertSql = new StringBuffer("insert into house_detail(house_id , house_address ,house_type ,house_usage ,house_area ,house_price , house_limit) values(");
+				insertSql.append("'" + houseDetail.getId() + "'");
+				insertSql.append(",'" + houseDetail.getAddress() + "'");
+				insertSql.append(",'" + houseDetail.getType() + "'");
+				insertSql.append(",'" + houseDetail.getUsage() + "'");
+				insertSql.append(",'" + houseDetail.getArea() + "'");
+				insertSql.append(",'" + houseDetail.getPrice() + "'");
+				insertSql.append(",'" + houseDetail.getLimit() + "'");
+				insertSql.append(")");
+				System.out.println(insertSql);
+
+				statement.execute(insertSql.toString());
+			}
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Set<String> getHouseIdSet(Connection connection) {
+		Set<String> set =new HashSet<String>();
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet query = statement.executeQuery("select house_id from house_detail");
+			while (query.next()) {
+				set.add(query.getString(1));
+			} 
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return set ;
+	}
+
+	
+	
+	/**
+	 * @return
+	 */
+	public static Connection getJDBCConnection() {
+		try {
+			return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "house", "password");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		;
+		return null;
+	}
+
+}
