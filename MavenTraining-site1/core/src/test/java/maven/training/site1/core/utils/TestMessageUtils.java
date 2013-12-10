2bb2d32c269786aa545023571ac7152d71ab94e5
gelnyang <gelnyang@gmail.com>
2013-12-10 10:20:58 +0000

    site1

---
+++ MavenTraining-site1/core/src/test/java/maven/training/site1/core/utils/TestMessageUtils.java
@@ -0,0 +1,12 @@
+package maven.training.site1.core.utils;
+
+import org.junit.Test;
+
+public class TestMessageUtils {
+
+	@Test
+	public void testSayHello() {
+		MessageUtils.sayHello("eric");
+	}
+
+}
